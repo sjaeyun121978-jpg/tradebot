09554f5fd22c5167f36e2b7c51cebbf3eba92544
sjaeyun121978-jpg <sjaeyun121978@gmail.com>
2026-04-24 09:07:49 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,25 @@
 
+import os
+
+TELEGRAM_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.environ.get("TG_CHAT_ID")
+
+TG_API_ID = int(os.environ.get("TG_API_ID"))
+TG_API_HASH = os.environ.get("TG_API_HASH")
+SESSION_STRING = os.environ.get("SESSION_STRING")
+
+BYBIT_API_KEY = os.environ.get("BYBIT_API_KEY")
+BYBIT_API_SECRET = os.environ.get("BYBIT_API_SECRET")
+
+ANTHROPIC_KEY = os.environ.get("ANTHROPIC_KEY")
+GOOGLE_SHEETS_KEY = os.environ.get("GOOGLE_SHEETS_KEY")
+GOOGLE_SHEETS_ID = os.environ.get("GOOGLE_SHEETS_ID")
+
+ENABLE_AUTO_TRADE = False
+DRY_RUN = True
+
+USDT_PER_TRADE = 5
+MAX_LEVERAGE = 5
+
+CLAUDE_MODEL = "claude-haiku-4-5-20251001"
+CLAUDE_TIMEOUT = 30.0
